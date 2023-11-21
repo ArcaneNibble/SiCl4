7fc0ffcb04c963f0edeff5e1184b291dc93edb94
R <rqou@berkeley.edu>
2023-11-21 18:15:21 +0000

    fix up repl; test creating module

--- sicl4_repl/src/main.rs
+++ sicl4_repl/src/main.rs
@@ -2,6 +2,18 @@ use std::{error::Error, fmt::Display};
 
 use pyo3::prelude::*;
 
+#[pyfunction]
+fn test_double(x: usize) -> usize {
+    x * 2
+}
+
+#[pymodule]
+#[pyo3(name = "sicl4")]
+fn sicl4_py_module(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(test_double, m)?)?;
+    Ok(())
+}
+
 #[derive(Debug)]
 enum ReplError {
     PythonError(PyErr),
@@ -32,10 +44,31 @@ impl From<std::io::Error> for ReplError {
 }
 
 fn main() -> Result<(), ReplError> {
+    pyo3::append_to_inittab!(sicl4_py_module);
+
     let args = std::env::args().collect::<Vec<_>>();
     if args.len() == 1 {
+        // https://stackoverflow.com/questions/7116038/python-repl-tab-completion-on-macos/7116997#7116997
+        // https://stackoverflow.com/questions/50917938/enabling-console-features-with-code-interact
+        let startup_code = r#"
+import code
+import readline
+import rlcompleter
+
+vars = globals()
+vars.update(locals())
+readline.set_completer(rlcompleter.Completer(vars).complete)
+
+if 'libedit' in readline.__doc__:
+    readline.parse_and_bind("bind ^I rl_complete")
+else:
+    readline.parse_and_bind("tab: complete")
+
+code.InteractiveConsole(vars).interact('Welcome to SiCl4 Python REPL!')
+        "#;
+
         Python::with_gil(|py| {
-            py.run("import code; code.interact('Welcome to SiCl4 Python REPL!')", None, None)
+            py.run(startup_code, None, None)
         })?;
     } else {
         let fn_to_load = &args[1];
